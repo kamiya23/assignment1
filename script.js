dba9949852d8a7e63f8e4566ce8a4f5cd480c5ce
kamiya garg <kamiya.garg@gmail.com>
2016-06-29 15:11:45 -0700

    Assignment 1

---
+++ script.js
@@ -0,0 +1,212 @@
+ /*//STEP 1
+ 
+var someMonth;       
+function theMonth(currentMonth)   //function to return current month 
+const currentMonth                //a constant
+var summerMonth;            // an array of summer months
+myLibrary ()                // a function 
+*/
+
+    
+ /*   //STEP 2
+ 
+ 12             //Numeric literal expression
+ "Kamiya"       //String literal expression
+ true           //Boolean literal expression
+ null           //null literal expression       
+ */
+
+
+/*  //STEP 3
+
+var example1 = 5 * (3 + 4);                     // example first for variable expressions
+
+var example2 = "Total counts are " + example1;      // Example second for variable Expressions   
+*/
+
+
+/*  //STEP 4            Variable declare with hungarian notification and camel case
+
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var intZip;
+var intYourAge;
+var strReferralSource;
+var blnMayWeContactYou;   
+*/
+
+
+
+
+/* //STEP 5
+var strFirstName = 'Kamiya';      //First method
+    
+var strlastName;
+strFirstName = 'Garg';            //Second Method
+                
+var strAddress = strFirstName + ' ' + strLastName + ' ' + strCity + ' ' + strState + ' ' + intZip;           //Third Method to assign 
+*/
+
+
+/*//STEP 6
+var x, y, z;
+x = 5 + " Hello";               //variable first display
+
+y = 5;
+y = (y < 10) + " Hello";        //variable second display
+
+
+z = 6;
+z = z + (z < 5);                // variable third display
+*/
+
+/*//STEP 7
+
+window.console.log(x);          // output is undefined with hoisting
+var x;
+*/
+
+/*//STEP 8
+// it was not valid. here i fixed it.
+var someString = 'Who once said, "Only two things are infinite, the universe andhuman stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+*/
+
+
+/*//STEP 9
+var x, y;
+x = null;
+window.console.log(x);
+window.console.log(y);
+*/
+
+/*//STEP 10
+var name = typeof "Kamiya";
+window.console.log(name);               // return string
+
+var age = typeof 21;
+window.console.log(age);                //return number
+
+var result = typeof false;
+window.console.log(result);             //return boolean
+
+
+var info = typeof {name:'Kamiya', city:'San Diego'}
+window.console.log(info);               //return object
+
+var x = typeof mobNum;
+window.console.log(x);                  // return undefined
+
+*/
+
+/*//Step 11
+ 
+window.alert('Hello' + ' Kamiya Garg' + ', welcome to the JavaScript class!');              // this alert box will display Hello Kamiya Garg, Welcome to the JavaScript class!
+
+*/
+
+/*//STEP 12
+var name = 'kamiya Garg';
+window.alert('Hello ' + name + ', welcomw to the JavaScript class!');
+*/
+
+/*//STEP 13
+var course = "JavaScript";
+window.alert(course);         //Alert box with text
+*/
+
+/*//STEP 14
+
+window.alert('Hello' + ' Kamiya Garg' + ',\n welcome to the JavaScript class!');
+*/
+
+/*//STEP 15
+var name = window.prompt("Enter Your Full Name : ");    //input from user and store in variable name
+
+window.console.log('Hello ' + name + ', welcome to the JavaScript class!');
+*/
+
+/*//STEP 16
+var course = window.prompt("Enter Your Class you are taking : ");    //input from user and store in variable name
+
+window.console.log('Hello ' + 'Kamiya Garg' + ', welcome to the ' + course + ' class!');
+*/
+
+/*//STEP 17
+var x, y;
+x = 10;
+y = 20;
+window.console.log(x + y);
+*/
+
+/*//STEP 18
+var x = 20;
+x = x + 20;
+window.console.log(x);
+*/
+
+/*//STEP 19
+var x;
+x = 20;
+x = x * 5;
+window.console.log(x);
+*/
+
+/*//STEP 20
+var x = 20 % 3;
+x = x / 1;
+window.console.log(x);
+*/
+
+
+/*//STEP 21
+
+var x = 5;
+if((x <= 10) || (x >= 5))
+    {
+    window.console.log('condition true');
+    } else
+        {
+            window.console.log('condition false');
+        }
+  // display true condition
+*/
+
+
+/*//STEP 22
+
+var num = 8;
+if ((num == 5) && (num != 0))
+    {
+        window.console.log("condition true");
+    }else {
+        window.console.log('Condition False');
+    }
+*/
+
+
+/*//STEP 23
+var widget = new Object();
+
+window.console.log(typeof widge); // display undefined
+*/
+
+
+/*//STEP 24
+var widget = ['kamiya', 'Kari', 'nimah'];
+
+window.console.log(widget instanceof Object);       //return true
+*/
+
+
+
+/*//STEP 25
+var widget = ['kamiya', 'Kari', 'nimah'];
+
+window.console.log(widget instanceof String);       //display false
+
+*/
+
